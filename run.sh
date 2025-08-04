76ef881d3344db1bca919c41cd53b46416a7d9d6
fuureya <agiljibrin009@gmail.com>
2025-08-04 10:45:34 +0800

    fix: laravel add

---
+++ run.sh
@@ -0,0 +1,2 @@
+docker compose -f production/docker-compose.yml up -d ;
+docker compose -f laravel/docker-compose.yml up -d --build
---
+++ stop.sh
@@ -0,0 +1,2 @@
+docker compose -f production/docker-compose.yml down ;
+docker compose -f laravel/docker-compose.yml down
